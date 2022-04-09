22e38371e5ace9ed288070d026005c78e227442e
Hristo Vasilev <hristo39@gmail.com>
2022-04-09 15:29:17 +0300

    another problem from basics

---
+++ Python/Past exams/sum_and_product.py
@@ -0,0 +1,32 @@
+num = int(input())
+is_combination = False
+
+for a in range(1, 10):
+	if is_combination:
+		break
+
+	for b in range(9, a, -1):
+		if is_combination:
+			break
+
+		for c in range(0, 10):
+			if is_combination:
+				break
+
+			for d in range(9, c, -1):
+
+				add_result = a+b+c+d
+				multiply_result = a*b*c*d
+
+				if add_result == multiply_result and num % 10 == 5:
+					print(f"{a}{b}{c}{d}")
+					is_combination = True
+					break
+				
+				elif multiply_result // add_result == 3 and num % 3 == 0:
+					print(f"{d}{c}{b}{a}")
+					is_combination = True12
+					break
+				
+if not is_combination:
+	print(f'Nothing found')
